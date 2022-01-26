ec1ea78820717761ab56769b55bfdb5314da83e5
js <11235.dev@gmail.com>
2022-01-26 18:06:16 +0900

    5-4 후위식 연산

--- inflearn/javaCodingTest/Q5_04후위식_연산.java
+++ inflearn/javaCodingTest/Q5_04후위식_연산.java
@@ -1,10 +1,40 @@
 package inflearn.javaCodingTest;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Q5_04후위식_연산 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
+
+        int result = evaluatePostfix(input);
+        System.out.println(result);
+    }
+
+    private static int evaluatePostfix(String input) {
+        Stack<Integer> stack = new Stack<>();
+        for (char ch : input.toCharArray()) {
+            if (Character.isDigit(ch)) stack.push(ch - '0');
+            else {
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                switch (ch) {
+                    case '+':
+                        stack.push(num1 + num2);
+                        break;
+                    case '-':
+                        stack.push(num1 - num2);
+                        break;
+                    case '*':
+                        stack.push(num1 * num2);
+                        break;
+                    case '/':
+                        stack.push(num1 / num2);
+                        break;
+                }
+            }
+        }
+        return stack.pop();
     }
 }
